e23234ad1d513a97473676a13be19fcbd78c3a6e
Marc Toussaint <toussaint@tu-berlin.de>
2024-10-10 20:51:33 +0200

    minor

--- src/BotOp/motionHelpers.h
+++ src/BotOp/motionHelpers.h
@@ -9,7 +9,7 @@ arr getLoopPath(rai::Configuration& C);
 void addBoxPickObjectives_botop(KOMO& komo, double time, rai::ArgWord dir, const char* boxName, const arr& boxSize, const char* gripperName, const char* palmName, const char* tableName);
 void addBoxPlaceObjectives_botop(KOMO& komo, double time, rai::ArgWord dir, const char* boxName, const arr& boxSize, const char* gripperName, const char* palmName);
 arr getBoxPnpKeyframes(const rai::Configuration& C,
-                       rai::ArgWord pickDirection, rai::ArgWord placeDirection,
+                       str pickDirection, str placeDirection,
                        const char* boxName, const char* gripperName, const char* palmName, const char* tableName,
                        const arr& qHome);
 arr getBoxPnpKeyframes_new(rai::Configuration& C, str graspDirection, str placeDirection, str box, str gripper, str palm, str table, const arr& qHome);
